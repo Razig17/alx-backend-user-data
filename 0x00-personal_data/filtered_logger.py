3513515b576d60ffca21d67f98317f4c83f3efcd
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-12 15:23:56 +0300

    update filter_datum function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,10 +11,8 @@ PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Obfuscate fileds inside a message"""
-    for field in fields:
-        message = re.sub(rf"{field}=.+?{separator}",
-                         f"{field}={redaction}{separator}", message)
-    return message
+    field = '|'.join(fields)
+    return re.sub(fr'({field})=[^{separator}]*', fr'\1={redaction}', message)
 
 
 class RedactingFormatter(logging.Formatter):
