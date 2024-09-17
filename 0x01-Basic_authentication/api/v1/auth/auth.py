48015d15a86f396947b2327363de6a56e58ad1b4
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-17 18:37:02 +0300

    Update require_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,6 +10,12 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Determines if authentication is required"""
+        if path is None or excluded_paths is None:
+            return True
+        if path[-1] != '/':
+            path = path + '/'
+        if path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
