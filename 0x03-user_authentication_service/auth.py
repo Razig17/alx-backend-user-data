e283c7c13fb61984b54fc5f4b6c56bc29859e1f8
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-25 16:20:25 +0300

    Implement the _generate_uuid method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -38,3 +39,7 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def _generate_uuid(self) -> str:
+        """Return a string representation of a new UUID"""
+        return uuid.uuid1()
