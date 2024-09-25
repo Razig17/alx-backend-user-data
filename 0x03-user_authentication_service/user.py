2dcb01a425192594fdd5cdb5c98fe82b8399d005
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-25 11:57:32 +0300

    Fix a typo

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -9,8 +9,8 @@ Base = declarative_base()
 
 class User(Base):
     """Users table class"""
-    __tablename__ = 'Users'
-    id = Column(Integer, primary_key=True,)
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
