596c4a81938407e612be940aa30a92c7f2c18ade
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-25 16:35:59 +0300

    Remove self parameter

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -41,6 +41,6 @@ class Auth:
             return False
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """Return a string representation of a new UUID"""
     return str(uuid.uuid4())
