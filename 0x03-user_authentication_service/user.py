770b7cc331ffefc4f07eb6ea88e472e9164577d5
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-25 11:55:06 +0300

    Add length to the attributes

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,8 +10,8 @@ Base = declarative_base()
 class User(Base):
     """Users table class"""
     __tablename__ = 'Users'
-    id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    session_id = Column(String, nullable=True)
-    rest_token = Column(String, nullable=True)
+    id = Column(Integer, primary_key=True,)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    rest_token = Column(String(250), nullable=True)
