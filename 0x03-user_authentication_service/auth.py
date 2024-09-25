e28581fc72a664d91126a3848262b5d392437c11
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-25 22:20:25 +0300

    Update the create_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,8 +47,7 @@ class Auth:
         except NoResultFound:
             return None
         session_id = _generate_uuid()
-        user.session_id = session_id
-        self._db._session.commit()
+        self._db.update_user(user.id, session_id=session_id)
         return session_id
 
     def get_user_from_session_id(self, session_id) -> User:
