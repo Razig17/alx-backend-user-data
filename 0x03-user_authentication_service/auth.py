5317ca9b3889f1c7d2386510034377c9875f7e54
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-26 20:21:34 +0300

    Update update_passwor method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,6 +80,8 @@ class Auth:
 
     def update_password(self, reset_token: str, password: str) -> None:
         """Updates a user's password"""
+        if reset_token is None:
+            return None
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
