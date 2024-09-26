aa0f789e6c97c5116f48eb1089c230302eeb95fa
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-26 09:21:53 +0300

    Update the end-point to Show user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -59,7 +59,7 @@ def profile():
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
-    return jsonify({"email": f"{user.email}"})
+    return jsonify({"email": user.email})
 
 
 if __name__ == "__main__":
