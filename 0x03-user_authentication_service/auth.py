014a7dea8aad5349a2ebb32e59f3cce14d77a249
Razig17 <abdelrazig.sharif1@gmail.com>
2024-09-26 19:10:48 +0300

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -11,7 +11,7 @@ import uuid
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password"""
-    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 class Auth:
@@ -44,14 +44,17 @@ class Auth:
         """Creates a session id"""
         try:
             user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
         except NoResultFound:
             return None
-        session_id = _generate_uuid()
-        self._db.update_user(user.id, session_id=session_id)
-        return session_id
+  
 
     def get_user_from_session_id(self, session_id) -> User:
         """Finds a user by session ID"""
+        if session_id is None:
+            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
             return user
